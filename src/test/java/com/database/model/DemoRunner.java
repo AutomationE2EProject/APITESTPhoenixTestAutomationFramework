022d4b7a70d6e9290e8b87ddb836ce5d21edd6e1
Jatin V Sharma <jatinvsharma@gmail.com>
2025-11-24 19:14:53 +0530

    Created CustomerDao and CustomerDBModel for Requirement 13

---
+++ src/test/java/com/database/model/DemoRunner.java
@@ -0,0 +1,11 @@
+package com.database.model;
+
+public class DemoRunner {
+	public static void main(String[] args) {
+
+		CustomerDBModel customer = new CustomerDBModel("Jatin", "Shharma", "7045663552", "", "jatinvshharma@gmail.com",
+				"");
+
+		System.out.println(customer);
+	}
+}
